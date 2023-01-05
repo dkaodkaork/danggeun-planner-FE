82a7dcaacfe67121bccb50471dfd2fdf0e2b8697
dkaodkaork <dkaodkaork@gmail.com>
2023-01-05 19:00:30 +0900

    feat: 로그인 회원가입 api 추가

--- src/core/api.jsx
+++ src/core/api.jsx
@@ -1,8 +1,6 @@
 import { instance } from "./apiConfig";
 
-// 예시
 export const api = {
-  // signup: (payload) => baseURL.post(WEBSERVICES.signup, payload),
-  // member
   postLoginApi: (loginInfo) => instance.post(`member/login`, loginInfo),
+  postSignUpApi: (signUpInfo) => instance.post(`member/signup`, signUpInfo),
 };
