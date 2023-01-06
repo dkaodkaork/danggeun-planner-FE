c3e050aea3b35917630256aea0634bbcbf10b653
DahyeJang <everything888@naver.com>
2023-01-06 16:00:12 +0900

    style : roboto 서체 추가

--- src/shared/HelmetComponent.jsx
+++ src/shared/HelmetComponent.jsx
@@ -5,10 +5,18 @@ class HelmetComponent extends Component {
   render() {
     return (
       <Helmet>
+        {/* Poppins 서체 */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+          rel="stylesheet"
+        />
+        {/* roboto 서체 */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
           rel="stylesheet"
         />
       </Helmet>
