363f625b5c843248ac08975a2ddf420efc65785e
dkaodkaork <dkaodkaork@gmail.com>
2023-01-06 14:33:16 +0900

    rename:파일 이름 수정

--- src/components/signUp/func.js
+++ src/components/signUp/func.js
@@ -9,9 +9,3 @@ export const isValidPassword = (pwd) => {
     /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,13}$/
   );
 };
-
-export const messages = {
-  emailInvalidMsg: "올바른 이메일 형식으로 작성해주세요.",
-  pwdInvalidMsg: "영문, 숫자, 특수문자가 모두 포함된 8~13자리로 작성해주세요.",
-  checkPwdInvalidMsg: "비밀번호가 일치하지 않습니다.",
-};
