a3ba4f2d9148572ce5e2013f59e192a9955f8aa7
DahyeJang <everything888@naver.com>
2023-01-07 20:53:38 +0900

    style: 타이머 페이지 버튼 컴포넌트 제작

--- src/components/timer/GetCarrotModal.jsx
+++ src/components/timer/GetCarrotModal.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import TimerButton from "./TimerButton";
 
 const GetCarrotModal = (props) => {
-  return <ModalBox onClick={props.onOpenModal}>모달박스 표시</ModalBox>;
+  return (
+    <ModalBox onClick={props.onOpenModal}>
+      <CarrotImg>당근이미지</CarrotImg>
+      <GetMsg>당근 1개를 수확했습니다!</GetMsg>
+      <TimerButton width="108px" height="44px">
+        확인
+      </TimerButton>
+    </ModalBox>
+  );
 };
 
 export default GetCarrotModal;
@@ -18,4 +27,9 @@ const ModalBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 22px;
 `;
+
+const CarrotImg = styled.div``;
+
+const GetMsg = styled.div``;
