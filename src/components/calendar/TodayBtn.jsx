66711231637d20fdcf4f54b0319c79ef461f00c9
DahyeJang <everything888@naver.com>
2023-01-07 16:13:10 +0900

    rename: TodayBtn 파일 경로 수정

---
+++ src/components/calendar/TodayBtn.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as TodayBtnIcon } from "../../assets/images/calendar/todayBtnIcon.svg";
+
+const TodayBtn = (props) => {
+  return (
+    <BtnLayout onClick={props.onClickToday}>
+      <TodayBtnIcon />
+      <BtnText>Today</BtnText>
+    </BtnLayout>
+  );
+};
+
+export default TodayBtn;
+
+const BtnLayout = styled.div`
+  height: 30px;
+  width: 112px;
+  border-radius: 12px;
+  padding: 8px, 26px, 8px, 26px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #fffdfa;
+  gap: 2px;
+`;
+
+const BtnText = styled.div`
+  font-family: "MaplestoryOTFLight";
+  font-size: 13px;
+  color: #4a8a51;
+`;
