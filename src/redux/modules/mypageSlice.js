95d43771b43f0fd51842b1f2584b2c6f0e9b3345
dkaodkaork <dkaodkaork@gmail.com>
2023-01-09 02:20:45 +0900

    feat: 마이페이지 slice 코드 작성

--- src/redux/modules/mypageSlice.js
+++ src/redux/modules/mypageSlice.js
@@ -3,23 +3,47 @@ import { api } from "../../core/api";
 
 const initialState = {
   data: {
-    email: "",
-    username: "",
+    email: "test@naver.com",
+    username: "테스트 닉네임",
     profileImage: "",
-    totalCarrot: "",
+    totalCarrot: "99",
   },
   isLoading: false,
   error: null,
 };
 
-export const __addNickname = createAsyncThunk(
+export const __putNickname = createAsyncThunk(
   "nickname/add",
   async (payload, thunkAPI) => {
     console.log(payload);
     try {
-      const { data } = await api.postNicknameApi(payload);
+      const { data } = await api.putNicknameApi(payload);
       console.log(data);
       return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __getUserInfo = createAsyncThunk(
+  "userinfo/get",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await api.getUserInfoApi();
+      return thunkAPI.fulfillWithValue();
+    } catch (error) {
+      // return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __putProfileImg = createAsyncThunk(
+  "profileImg/put",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await api.putProfileImgApi(payload);
+      return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -31,12 +55,51 @@ export const mypageSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    // builder.addCase(__addTag.fulfilled, (state, action) => {
-    //   state.isLoading = false;
-    //   state.data = [...state.data, action.payload.data];
-    // });
+    builder
+
+      // 닉네임 등록
+      .addCase(__putNickname.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(__putNickname.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log(action);
+        // state.data =
+      })
+      .addCase(__putNickname.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // 마이페이지 유저정보 가져오기
+      .addCase(__getUserInfo.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(__getUserInfo.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log(action);
+        // state.data =
+      })
+      .addCase(__getUserInfo.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // 프로필이미지 변경하기
+      .addCase(__putProfileImg.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(__putProfileImg.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log(action);
+        // state.data =
+      })
+      .addCase(__putProfileImg.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const {} = mypageSlice.actions;
+// export const {} = mypageSlice.actions;
 export default mypageSlice.reducer;
