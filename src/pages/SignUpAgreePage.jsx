f833ed0a75fc9dfa871329cb8d9a2e679cee6801
dkaodkaork <dkaodkaork@gmail.com>
2023-01-09 02:17:05 +0900

    remove:  불필요 파일 삭제

--- src/pages/SignUpAgreePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import SignUpAgree from "../components/signUpAgree/SignUpAgree";
-
-const SignUpAgreePage = () => {
-  return <SignUpAgree></SignUpAgree>;
-};
-
-export default SignUpAgreePage;
