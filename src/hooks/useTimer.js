001b8355f6b9b5f824ca3caa464ffaa74f036ce9
dkaodkaork <dkaodkaork@gmail.com>
2023-01-09 19:51:52 +0900

    fix: 타이머 재실행시 버그수정

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -8,9 +8,6 @@ const useTimer = (callback, delay) => {
   const [isClear, setIsClear] = useState(true); //
   const [parsedTime, setParsedTime] = useState(""); // 타이머 시간 파싱 mm:ss
 
-  // console.log(delay);
-  // console.log(currentTime);
-
   useInterval(
     // 2) delay마다  실행하는 콜백함수는 무엇이냐 =>  1초마다 currenttime이 0보다 크면, 즉 설정한 타이머 시간이 존재할때, currenttime값에서 1초씩 빼라//타이머 기능
     () => {
@@ -25,7 +22,7 @@ const useTimer = (callback, delay) => {
   );
 
   useEffect(() => {
-    // currenttime이 0이되면 즉 타이머가 (0초) 끝나면, 컴포넌트에서 인자로 받은 콜백함수를 실행해라 => 즉 타이머가 끝났을때 실행할 로직을 받아야 겠지요?
+    // currenttime이 0이되면 즉 타이머가 (0초) 끝나면, 컴포넌트에서 인자로 받은 콜백함수를 실행해라 => 즉 타이머가 끝났을때 실행할 로직
     if (currentTime === 0) {
       callback();
       setIsClear(true);
