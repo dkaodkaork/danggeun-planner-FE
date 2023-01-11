c5359d56e2d10bdf541313246d03c31cbc74fcc1
dkaodkaork <dkaodkaork@gmail.com>
2023-01-11 11:11:54 +0900

    feat: 코드 수정

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,8 +2,6 @@ import { ReactComponent as Menu } from "../assets/images/menuicon.svg";
 import { ReactComponent as Home } from "../assets/images/home.svg";
 
 export const IMAGES = {
-  test: require("../assets/images/ee.jpeg"),
-  test1: require("../assets/images/timer.png"),
   carrotLv0: require("../assets/images/carrotLv0.svg"),
   carrotLv1: require("../assets/images/carrotLv1.svg"),
   carrotLv2: require("../assets/images/carrotLv2.svg"),
