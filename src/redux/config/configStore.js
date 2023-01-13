360460791b62199972ec0c01bf61f45bc0e122e2
dkaodkaork <dkaodkaork@gmail.com>
2023-01-14 00:15:52 +0900

    feat:planner store 연결

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import calendarSlice from "../modules/calendarSlice";
 import mypage from "../modules/mypageSlice";
 import timer from "../modules/timerSlice";
+import planner from "../modules/plannerSlice";
 
 const store = configureStore({
-  reducer: { mypage, calendarSlice, timer },
+  reducer: { mypage, calendarSlice, timer, planner },
 });
 
 export default store;
