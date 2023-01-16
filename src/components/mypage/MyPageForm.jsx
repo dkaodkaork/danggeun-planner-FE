f5d6c8d129c0fd8ed465fbbf5df778592c19c45e
dkaodkaork <dkaodkaork@gmail.com>
2023-01-17 06:47:55 +0900

    style: 마이페이지 레이아웃 구현

--- src/components/mypage/MyPageForm.jsx
+++ src/components/mypage/MyPageForm.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import Header from "../header/Header";
-import { PATH } from "../../constants/index";
+import Button from "../timer/TimerButton";
+import { PATH, IMAGES } from "../../constants/index";
 
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -26,109 +27,208 @@ const MypageForm = () => {
   };
 
   return (
-    <div>
-      <Header menuName="My"></Header>
+    <StContainer>
+      <Header
+        menuName="MY"
+        right={IMAGES.menu}
+        left={IMAGES.home}
+        leftLink={PATH.timer}
+      ></Header>
+      <Header
+        menuName="마이페이지"
+        right={IMAGES.edit}
+        left={IMAGES.fold}
+        leftLink={PATH.timer}
+      ></Header>
+
       <StProfileContainer>
         <StProfileBox>
           <StImg src={userInfo.profileImage}></StImg>
           <StInfoBox>
-            <span>{userInfo.username}</span>
-            <span>{userInfo.email}</span>
-            <Link to={PATH.profile}>
-              <button>프로필수정</button>
-            </Link>
+            <StInfo>{userInfo.username}</StInfo>
+            <StTotalCarrot>
+              <p>누적</p> <span>{IMAGES.carrotSticker}</span>
+              {userInfo.totalCarrot}
+            </StTotalCarrot>
           </StInfoBox>
         </StProfileBox>
-        <StBox>
-          <p>누적당근</p> <p>{userInfo.totalCarrot} 개</p>
-        </StBox>
+        <StHr></StHr>
+        <StEmailBox>{userInfo.email}</StEmailBox>
+        <StBtns>
+          <Button
+            border="1px solid #F1E5D2"
+            height="54px"
+            width="319px"
+            backgroundColor="#FFFDFA"
+            fontSize="1.4rem"
+            color="#595550"
+            fontFamily="Pretendard"
+            fontStyle="normal"
+            fontWeight="500"
+            gap="24px"
+            filter="none"
+            type="button"
+            onClick={() =>
+              window.open(
+                "https://www.notion.so/4de1bdd12eda48e79ac2dde2c45d6c8f"
+              )
+            }
+          >
+            도움말
+          </Button>
+          <Button
+            border="1px solid #F1E5D2"
+            height="54px"
+            width="319px"
+            backgroundColor="#FFFDFA"
+            fontSize="1.4rem"
+            fontFamily="Pretendard"
+            fontStyle="normal"
+            fontWeight="500"
+            color="#595550"
+            gap="25px"
+            filter="none"
+            onClick={() =>
+              window.open(
+                "https://www.notion.so/4de1bdd12eda48e79ac2dde2c45d6c8f"
+              )
+            }
+          >
+            1:1 문의
+          </Button>
+          <Button
+            border="1px solid #F1E5D2"
+            height="54px"
+            width="319px"
+            backgroundColor="#FFFDFA"
+            fontSize="1.4rem"
+            fontFamily="Pretendard"
+            fontStyle="normal"
+            fontWeight="500"
+            color="#FF0000"
+            gap="28px"
+            filter="none"
+            onClick={logoutHandler}
+          >
+            로그아웃
+          </Button>
+        </StBtns>
       </StProfileContainer>
-      <StButtonContainer>
-        <button
-          type="button"
-          onClick={() =>
-            window.open(
-              "https://www.notion.so/4de1bdd12eda48e79ac2dde2c45d6c8f"
-            )
-          }
-        >
-          도움말
-        </button>
-        <button
-          type=" button"
-          onClick={() =>
-            window.open(
-              "https://www.notion.so/4de1bdd12eda48e79ac2dde2c45d6c8f"
-            )
-          }
-        >
-          1:1 문의
-        </button>
-        <button onClick={logoutHandler}>로그아웃</button>
-      </StButtonContainer>
-    </div>
+    </StContainer>
   );
 };
 
 export default MypageForm;
 
+const StContainer = styled.div`
+  background-color: #f9f3ea;
+  height: 812px;
+`;
+
 const StProfileContainer = styled.div`
-  border: 1px solid black;
-  margin: 40px 10px 40px 10px;
+  width: 375px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
 `;
 
 const StProfileBox = styled.div`
+  width: 319px;
+  height: 125px;
+  margin-top: 12px;
   display: flex;
   flex-direction: row;
-  align-items: center;
-  margin: 10px;
-  padding: 20px;
-  gap: 15px;
-
-  border-bottom: 1px solid black;
 `;
 
 const StImg = styled.img`
-  border-radius: 50%;
+  border-radius: 100%;
   object-fit: cover;
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
+  width: 125px;
+  height: 125px;
+  border: 0px;
+  /* display: inline-block;
+  content: ""; */
+  /* border: 1px solid black; */
 `;
 
 const StInfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin: 20px;
+  width: 162px;
+`;
+const StInfo = styled.div`
+  width: 126px;
+  height: 27px;
 
-  button {
-    border: 1px solid black;
-  }
+  font-family: "MaplestoryOTFBold";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 27px;
+
+  margin: 70px 24px 10px 24px;
+
+  color: #595550;
 `;
 
-const StBox = styled.div`
+const StTotalCarrot = styled.div`
   display: flex;
-  justify-content: space-around;
-  gap: 10px;
-  margin: 20px;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
+  text-align: center;
+  gap: 6.4px;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.6rem;
+
+  color: #f27808;
 
   p {
-    text-align: center;
-    width: 100px;
+    margin-left: 24px;
+    width: 25px;
+    height: 22px;
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.4rem;
+    padding-top: 2px;
+
+    color: #403b36;
   }
 `;
 
-const StButtonContainer = styled.div`
-  border: 1px solid black;
-  margin: 40px 10px 40px 10px;
-  padding: 10px;
+const StHr = styled.hr`
+  width: 319px;
+  opacity: 0.1;
+  border: 1px solid #000000;
+  margin: 24px 0px 12px 0px;
+`;
+
+const StEmailBox = styled.div`
+  width: 319px;
+  height: 22px;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 160%;
+  /* or 22px */
+
+  display: flex;
+  align-items: center;
+
+  /* word2 */
+
+  color: #a4a4a4;
+`;
+
+const StBtns = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
-
-  button {
-    border: 1px solid black;
-    height: 30px;
-  }
+  gap: 10px;
+  margin-top: 87px;
 `;
