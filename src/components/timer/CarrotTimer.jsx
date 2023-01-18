d37aece2108edfe6da5e3758134782f7dbcc31ae
dkaodkaork <dkaodkaork@gmail.com>
2023-01-18 22:23:45 +0900

    feat: 사이드 메뉴 관련 코드 추가

--- src/components/timer/CarrotTimer.jsx
+++ src/components/timer/CarrotTimer.jsx
@@ -6,16 +6,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { __startTimer, __finsihTimer } from "../../redux/modules/timerSlice";
 import TimerBackground from "./TimerBackground";
 import { timeStamp } from "../planner/time";
+import { IMAGES } from "../../constants/index";
+import Header from "../header/Header";
+
+//
+import { groupMenuOpenStatus } from "../../redux/modules/modalSlice";
+import GetCarrot from "./GetCarrot";
 
 const CarrotTimer = () => {
   const [stack, setStack] = useState("");
   const [mode, setMode] = useState("focusMode");
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   const dispatch = useDispatch();
 
   const data = useSelector((state) => state.timer.data);
-  // console.log(data);
+
+  // 메뉴 오픈 관련 추후에 반드시 빼야함
+  const groupMenuOpen = useSelector((state) => state.modalSlice.groupMenuOpen);
+
+  const clickGroupMenuHandler = () => {
+    dispatch(groupMenuOpenStatus(!groupMenuOpen));
+  };
+  //
 
   const startTime = 1000 * 4;
   const restTime = 1000 * 2;
@@ -28,14 +41,18 @@ const CarrotTimer = () => {
 
   useLayoutEffect(() => {
     if (mode === "focusMode") {
-      if (currentTime <= 4000 && currentTime > 3000) {
+      if (currentTime <= 80000 && currentTime > 70000) {
         setStack("default");
-      } else if (currentTime <= 3000 && currentTime > 2000) {
+      } else if (currentTime <= 70000 && currentTime > 60000) {
         setStack("step1");
-      } else if (currentTime <= 2000 && currentTime > 1000) {
+      } else if (currentTime <= 60000 && currentTime > 50000) {
         setStack("step2");
-      } else if (currentTime <= 1000 && currentTime > 0) {
+      } else if (currentTime <= 50000 && currentTime > 40000) {
         setStack("step3");
+      } else if (currentTime <= 40000 && currentTime > 30000) {
+        setStack("step4");
+      } else if (currentTime <= 30000 && currentTime > 20000) {
+        setStack("step5");
       } else {
         setStack("step6");
       }
@@ -53,19 +70,22 @@ const CarrotTimer = () => {
     }
   };
 
+  console.log(count);
+
   const focusModeDoneHandler = () => {
     toggleTimer(0);
-    // setCount(count + 1);
-    const timerInfo = { endTime: timeStamp(), count: count };
+
+    const timerInfo = { endTime: timeStamp(), count: count + 1 };
 
     dispatch(__finsihTimer({ timerId: data.timerId, timerInfo }));
+
     // console.log(timeStamp());
   };
 
   const restModeDoneHandler = () => {
-    if (count === 3) {
+    if (count === 2) {
       toggleTimer(longRestTime);
-      setCount(1);
+      setCount(0);
     } else {
       toggleTimer(startTime);
       setMode("focusMode");
@@ -74,7 +94,7 @@ const CarrotTimer = () => {
 
   const startTimerHandler = () => {
     toggleTimer();
-    if (timer === startTime) {
+    if (timer === startTime && count === 0) {
       dispatch(__startTimer({ startTime: timeStamp() }));
       console.log("타이머 시작통신");
       // console.log(timeStamp());
@@ -83,7 +103,7 @@ const CarrotTimer = () => {
 
   const focusGiveUpHandler = () => {
     toggleTimer(startTime);
-    setCount(1);
+    setCount(0);
   };
 
   const getCarrotHandler = () => {
@@ -93,15 +113,11 @@ const CarrotTimer = () => {
     // 모달만 다시 해결해보자  모달안에 스타트 타이머 넣어서 적용해보자
   };
 
-  // const 휴식startTimerHandler = () => {
-  //   toggleTimer(restTime);
-  // };
-
   const focusMode = {
     start: currentTime ? (
       <Button onClick={startTimerHandler}>집중시작하기</Button>
     ) : (
-      <Button onClick={getCarrotHandler}>당근 수확하기</Button>
+      <GetCarrot onClick={getCarrotHandler}>당근 수확하기</GetCarrot>
     ),
     rest: (
       <Button
@@ -161,17 +177,26 @@ const CarrotTimer = () => {
   };
 
   return (
-    <StContainer>
-      <TimerBackground
-        parsedTime={parsedTime}
-        isClear={isClear}
-        perBtnByMode={perBtnByMode}
-        mode={mode}
-        stack={stack}
-        strokeDashoffset={strokeDashoffset}
-        circumference={circumference}
-      ></TimerBackground>
-    </StContainer>
+    <>
+      <Header
+        right={IMAGES.menu}
+        menuName="TIMER"
+        marginLeft="32px"
+        clickMenuHandler={clickGroupMenuHandler}
+      />
+      <StContainer>
+        <TimerBackground
+          parsedTime={parsedTime}
+          isClear={isClear}
+          perBtnByMode={perBtnByMode}
+          mode={mode}
+          stack={stack}
+          strokeDashoffset={strokeDashoffset}
+          circumference={circumference}
+        ></TimerBackground>
+      </StContainer>
+      {/* <GetCarrot onClick={} /> */}
+    </>
   );
 };
 
