bb35745ef24bd219c85ae51e26eb64ff7c490b53
dkaodkaork <dkaodkaork@gmail.com>
2023-01-25 20:21:20 +0900

    feat: 헤더 컴포넌트 임시 추가

---
+++ src/components/header/Head.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Head = ({ leftLink, leftSlot, title, rightSlot, onClick }) => {
+  return (
+    <StContainer>
+      <StBox>
+        <StLeftSlot>
+          <Link to={leftLink}>{leftSlot}</Link>
+        </StLeftSlot>
+        <StCenterSlot>{title}</StCenterSlot>
+        <StRightSlot onClick={onClick}>{rightSlot}</StRightSlot>
+      </StBox>
+    </StContainer>
+  );
+};
+
+export default Head;
+
+const StContainer = styled.div`
+  width: 375px;
+  height: 72px;
+  background-color: #f9f3ea;
+  padding: 28px 28px 12px 28px;
+`;
+
+const StBox = styled.div`
+  width: 319px;
+  height: 32px;
+  /* border: 1px solid black; */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StLeftSlot = styled.div`
+  width: 32px;
+  height: 32px;
+  /* border: 1px solid black; */
+`;
+
+const StCenterSlot = styled.div`
+  width: 219px;
+  height: 18px;
+  font-family: "MaplestoryOTFLight";
+  font-size: 1.4rem;
+  line-height: 18px;
+  text-align: center;
+  color: #595550;
+`;
+
+const StRightSlot = styled.div`
+  width: 32px;
+  height: 32px;
+  /* border: 1px solid black; */
+`;
