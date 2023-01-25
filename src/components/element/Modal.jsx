20d53c414cf0369de1112af78f5dae0f129fc073
DahyeJang <everything888@naver.com>
2023-01-25 23:54:41 +0900

    feat: 모달 컴포넌트 리팩토링

--- src/components/element/Modal.jsx
+++ src/components/element/Modal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const Modal = ({ children, height, width }) => {
+const Modal = ({ children, height, width, modalRef, modalOutSideClick }) => {
   return (
-    <ModalBackdrop>
+    <ModalBackdrop ref={modalRef} onClick={(e) => modalOutSideClick(e)}>
       <ModalBox height={height} width={width}>
         {children}
       </ModalBox>
