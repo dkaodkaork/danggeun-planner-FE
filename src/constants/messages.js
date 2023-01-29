b1c10f43d1a8b4df9b5a6d70db9deb8c2732dc53
dkaodkaork <dkaodkaork@gmail.com>
2023-01-30 00:45:39 +0900

    feat: alert 오류 메세지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,4 +4,12 @@ export const MSG = {
   pwdInvalidMsg: "영문, 숫자, 특수문자가 모두 포함된 8~13자리로 작성해주세요.",
   checkPwdInvalidMsg: "비밀번호가 일치하지 않습니다.",
   formInvalidMsg: "이메일과, 비밀번호를 모두 입력해주세요!",
+
+  // 플래너
+  planEmptyMsg: "제목과 시간을 모두 입력해주세요!",
+  timeErrMsg: "올바른 시간을 입력해주세요!",
+  hourErrMsg: "입력 가능한 시작시간은 00~23시 입니다!",
+  minErrMsg: "60분을 넘어갈 수 없습니다!",
+  //
+  titleEmptyMsg: "제목을 입력해주세요!",
 };
