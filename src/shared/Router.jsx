0d5e7511ae569e0e2e1ce529ce38c3927f24d750
dkaodkaork <dkaodkaork@gmail.com>
2023-02-04 22:16:56 +0900

    refactor: 웹페이지 성능 개선

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 
-import LoginPage from "../pages/auth/LoginPage";
-import SignUpPage from "../pages/auth/SignUpPage";
-import CalendarPage from "../pages/calendar/CalendarPage";
-import UsernameFormPage from "../pages/auth/UsernameFormPage";
-import MyPage from "../pages/user/MyPage";
-import ProfileEditPage from "../pages/user/ProfileEditPage";
-import TimerPage from "../pages/timer/TimerPage";
-import GetCarrotPage from "../pages/timer/GetCarrotPage";
-import PlannerPage from "../pages/planner/PlannerPage";
-import GroupListPage from "../pages/group/GroupListPage";
-import GroupAddPage from "../pages/group/GroupAddPage";
-import GroupDetailPage from "../pages/group/GroupDetailPage";
-import GroupUpdatePage from "../pages/group/GroupUpdatePage";
-import GroupInvitePage from "../pages/group/GroupInvitePage";
-import AlarmPage from "../pages/alarm/AlarmPage";
-import TutorialPage from "../pages/tutorial/TutorialPage";
-import IntroPage from "../pages/auth/IntroPage";
-import KakaoLoginPage from "../pages/auth/KakaoLoginPage";
-import RouteChangeTracker from "./RouterChangeTracker";
-import ErrorPage from "../pages/error/ErrorPage";
-import { useState } from "react";
+// import LoginPage from "../pages/auth/LoginPage";
+const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
+
+// import SignUpPage from "../pages/auth/SignUpPage";
+const SignUpPage = lazy(() => import("../pages/auth/SignUpPage"));
+
+// import CalendarPage from "../pages/calendar/CalendarPage";
+const CalendarPage = lazy(() => import("../pages/calendar/CalendarPage"));
+
+// import UsernameFormPage from "../pages/auth/UsernameFormPage";
+const UsernameFormPage = lazy(() => import("../pages/auth/UsernameFormPage"));
+
+// import MyPage from "../pages/user/MyPage";
+const MyPage = lazy(() => import("../pages/user/MyPage"));
+
+// import ProfileEditPage from "../pages/user/ProfileEditPage";
+const ProfileEditPage = lazy(() => import("../pages/user/ProfileEditPage"));
+
+// import TimerPage from "../pages/timer/TimerPage";
+const TimerPage = lazy(() => import("../pages/timer/TimerPage"));
+
+// import PlannerPage from "../pages/planner/PlannerPage";
+const PlannerPage = lazy(() => import("../pages/planner/PlannerPage"));
+
+// import GroupListPage from "../pages/group/GroupListPage";
+const GroupListPage = lazy(() => import("../pages/group/GroupListPage"));
+
+// import GroupAddPage from "../pages/group/GroupAddPage";
+const GroupAddPage = lazy(() => import("../pages/group/GroupAddPage"));
+
+// import GroupDetailPage from "../pages/group/GroupDetailPage";
+const GroupDetailPage = lazy(() => import("../pages/group/GroupDetailPage"));
+
+// import GroupUpdatePage from "../pages/group/GroupUpdatePage";
+const GroupUpdatePage = lazy(() => import("../pages/group/GroupUpdatePage"));
+
+// import GroupInvitePage from "../pages/group/GroupInvitePage";
+const GroupInvitePage = lazy(() => import("../pages/group/GroupInvitePage"));
+
+// import AlarmPage from "../pages/alarm/AlarmPage";
+const AlarmPage = lazy(() => import("../pages/alarm/AlarmPage"));
+
+// import TutorialPage from "../pages/tutorial/TutorialPage";
+const TutorialPage = lazy(() => import("../pages/tutorial/TutorialPage"));
+
+// import IntroPage from "../pages/auth/IntroPage";
+const IntroPage = lazy(() => import("../pages/auth/IntroPage"));
+
+// import KakaoLoginPage from "../pages/auth/KakaoLoginPage";
+const KakaoLoginPage = lazy(() => import("../pages/auth/KakaoLoginPage"));
+
+// import RouteChangeTracker from "./RouterChangeTracker";
+const RouteChangeTracker = lazy(() => import("./RouterChangeTracker"));
+
+// import ErrorPage from "../pages/error/ErrorPage";
+const ErrorPage = lazy(() => import("../pages/error/ErrorPage"));
 
 const Router = () => {
   const item = localStorage.getItem("accessToken");
@@ -43,43 +78,48 @@ const Router = () => {
 
   return (
     <BrowserRouter>
-      <RouteChangeTracker />
-      <Routes>
-        {accessToken ? (
-          <Route>
-            <Route path="/" element={<TimerPage />} />
-            <Route path="/username" element={<UsernameFormPage />} />
-            <Route path="/alarm" element={<AlarmPage />} />
-            <Route path="/calendar/:username" element={<CalendarPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/profile" element={<ProfileEditPage />} />
-            <Route path="/planner/:username/:date" element={<PlannerPage />} />
-            <Route path="/group" element={<GroupListPage />} />
-            <Route path="/group/add" element={<GroupAddPage />} />
-            <Route path="/group/:groupId" element={<GroupDetailPage />} />
-            <Route path="/error" element={<ErrorPage />} />
-            <Route
-              path="/group/:groupId/update"
-              element={<GroupUpdatePage />}
-            />
-            <Route
-              path="/group/:groupId/invite"
-              element={<GroupInvitePage />}
-            />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        ) : (
-          <Route>
-            <Route path="/tutorial" element={<TutorialPage />} />
-            <Route path="/" element={<IntroPage />} />
-            <Route path="/error" element={<ErrorPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/kakao/login" element={<KakaoLoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        )}
-      </Routes>
+      <Suspense fallback={<div>로딩중입니다</div>}>
+        {/* <RouteChangeTracker /> */}
+        <Routes>
+          {accessToken ? (
+            <Route>
+              <Route path="/" element={<TimerPage />} />
+              <Route path="/username" element={<UsernameFormPage />} />
+              <Route path="/alarm" element={<AlarmPage />} />
+              <Route path="/calendar/:username" element={<CalendarPage />} />
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/profile" element={<ProfileEditPage />} />
+              <Route
+                path="/planner/:username/:date"
+                element={<PlannerPage />}
+              />
+              <Route path="/group" element={<GroupListPage />} />
+              <Route path="/group/add" element={<GroupAddPage />} />
+              <Route path="/group/:groupId" element={<GroupDetailPage />} />
+              <Route path="/error" element={<ErrorPage />} />
+              <Route
+                path="/group/:groupId/update"
+                element={<GroupUpdatePage />}
+              />
+              <Route
+                path="/group/:groupId/invite"
+                element={<GroupInvitePage />}
+              />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          ) : (
+            <Route>
+              <Route path="/tutorial" element={<TutorialPage />} />
+              <Route path="/" element={<IntroPage />} />
+              <Route path="/error" element={<ErrorPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/kakao/login" element={<KakaoLoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          )}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
