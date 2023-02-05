285d27d1cbd1f389db640e982859ec113fea414f
dkaodkaork <dkaodkaork@gmail.com>
2023-02-05 20:40:49 +0900

    feat: router 코드 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,62 +2,62 @@ import React, { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect, Suspense } from "react";
 
-// import LoginPage from "../pages/auth/LoginPage";
-const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
+import LoginPage from "../pages/auth/LoginPage";
+// const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
 
-// import SignUpPage from "../pages/auth/SignUpPage";
-const SignUpPage = lazy(() => import("../pages/auth/SignUpPage"));
+import SignUpPage from "../pages/auth/SignUpPage";
+// const SignUpPage = lazy(() => import("../pages/auth/SignUpPage"));
 
-// import CalendarPage from "../pages/calendar/CalendarPage";
-const CalendarPage = lazy(() => import("../pages/calendar/CalendarPage"));
+import CalendarPage from "../pages/calendar/CalendarPage";
+// const CalendarPage = lazy(() => import("../pages/calendar/CalendarPage"));
 
-// import UsernameFormPage from "../pages/auth/UsernameFormPage";
-const UsernameFormPage = lazy(() => import("../pages/auth/UsernameFormPage"));
+import UsernameFormPage from "../pages/auth/UsernameFormPage";
+// const UsernameFormPage = lazy(() => import("../pages/auth/UsernameFormPage"));
 
-// import MyPage from "../pages/user/MyPage";
-const MyPage = lazy(() => import("../pages/user/MyPage"));
+import MyPage from "../pages/user/MyPage";
+// const MyPage = lazy(() => import("../pages/user/MyPage"));
 
-// import ProfileEditPage from "../pages/user/ProfileEditPage";
-const ProfileEditPage = lazy(() => import("../pages/user/ProfileEditPage"));
+import ProfileEditPage from "../pages/user/ProfileEditPage";
+// const ProfileEditPage = lazy(() => import("../pages/user/ProfileEditPage"));
 
-// import TimerPage from "../pages/timer/TimerPage";
-const TimerPage = lazy(() => import("../pages/timer/TimerPage"));
+import TimerPage from "../pages/timer/TimerPage";
+// const TimerPage = lazy(() => import("../pages/timer/TimerPage"));
 
-// import PlannerPage from "../pages/planner/PlannerPage";
-const PlannerPage = lazy(() => import("../pages/planner/PlannerPage"));
+import PlannerPage from "../pages/planner/PlannerPage";
+// const PlannerPage = lazy(() => import("../pages/planner/PlannerPage"));
 
-// import GroupListPage from "../pages/group/GroupListPage";
-const GroupListPage = lazy(() => import("../pages/group/GroupListPage"));
+import GroupListPage from "../pages/group/GroupListPage";
+// const GroupListPage = lazy(() => import("../pages/group/GroupListPage"));
 
-// import GroupAddPage from "../pages/group/GroupAddPage";
-const GroupAddPage = lazy(() => import("../pages/group/GroupAddPage"));
+import GroupAddPage from "../pages/group/GroupAddPage";
+// const GroupAddPage = lazy(() => import("../pages/group/GroupAddPage"));
 
-// import GroupDetailPage from "../pages/group/GroupDetailPage";
-const GroupDetailPage = lazy(() => import("../pages/group/GroupDetailPage"));
+import GroupDetailPage from "../pages/group/GroupDetailPage";
+// const GroupDetailPage = lazy(() => import("../pages/group/GroupDetailPage"));
 
-// import GroupUpdatePage from "../pages/group/GroupUpdatePage";
-const GroupUpdatePage = lazy(() => import("../pages/group/GroupUpdatePage"));
+import GroupUpdatePage from "../pages/group/GroupUpdatePage";
+// const GroupUpdatePage = lazy(() => import("../pages/group/GroupUpdatePage"));
 
-// import GroupInvitePage from "../pages/group/GroupInvitePage";
-const GroupInvitePage = lazy(() => import("../pages/group/GroupInvitePage"));
+import GroupInvitePage from "../pages/group/GroupInvitePage";
+// const GroupInvitePage = lazy(() => import("../pages/group/GroupInvitePage"));
 
-// import AlarmPage from "../pages/alarm/AlarmPage";
-const AlarmPage = lazy(() => import("../pages/alarm/AlarmPage"));
+import AlarmPage from "../pages/alarm/AlarmPage";
+// const AlarmPage = lazy(() => import("../pages/alarm/AlarmPage"));
 
-// import TutorialPage from "../pages/tutorial/TutorialPage";
-const TutorialPage = lazy(() => import("../pages/tutorial/TutorialPage"));
+import TutorialPage from "../pages/tutorial/TutorialPage";
+// const TutorialPage = lazy(() => import("../pages/tutorial/TutorialPage"));
 
-// import IntroPage from "../pages/auth/IntroPage";
-const IntroPage = lazy(() => import("../pages/auth/IntroPage"));
+import IntroPage from "../pages/auth/IntroPage";
+// const IntroPage = lazy(() => import("../pages/auth/IntroPage"));
 
-// import KakaoLoginPage from "../pages/auth/KakaoLoginPage";
-const KakaoLoginPage = lazy(() => import("../pages/auth/KakaoLoginPage"));
+import KakaoLoginPage from "../pages/auth/KakaoLoginPage";
+// const KakaoLoginPage = lazy(() => import("../pages/auth/KakaoLoginPage"));
 
-// import RouteChangeTracker from "./RouterChangeTracker";
-const RouteChangeTracker = lazy(() => import("./RouterChangeTracker"));
+import RouteChangeTracker from "./RouterChangeTracker";
+// const RouteChangeTracker = lazy(() => import("./RouterChangeTracker"));
 
-// import ErrorPage from "../pages/error/ErrorPage";
-const ErrorPage = lazy(() => import("../pages/error/ErrorPage"));
+import ErrorPage from "../pages/error/ErrorPage";
+// const ErrorPage = lazy(() => import("../pages/error/ErrorPage"));
 
 const Router = () => {
   const item = localStorage.getItem("accessToken");
