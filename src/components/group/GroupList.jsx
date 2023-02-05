38a6875bf30cc19510fbc7961afb4ce1bfa3cf57
DahyeJang <everything888@naver.com>
2023-02-05 20:21:35 +0900

    feat: 그룹 리스트 오류 수정

--- src/components/group/GroupList.jsx
+++ src/components/group/GroupList.jsx
@@ -22,10 +22,13 @@ const GroupList = () => {
     dispatch(__getGroupList());
   }, []);
 
+  //"2024년\uD83D\uDC07\uD83D\uDC07"
+  //이모티콘 정규식
+  const regex =
+    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+
   //이모티콘 공백으로 바꿈
   const removeEmojis = (str) => {
-    const regex =
-      /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     if (regex.test(str)) {
       return str.replace(regex, "");
     } else {
@@ -35,8 +38,6 @@ const GroupList = () => {
 
   //이모티콘 체크하고 반환
   const checkEmojis = (str) => {
-    const regex =
-      /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
     return regex.test(str);
   };
 
@@ -45,8 +46,11 @@ const GroupList = () => {
       return str;
     } else if (checkEmojis(str)) {
       const first = str.slice(0, 5);
+      if (checkEmojis(first[4])) {
+        removeEmojis(first[4]);
+      }
       const aa = str.slice(5, 8);
-      const strSliceRemove = removeEmojis(aa);
+      const strSliceRemove = removeEmojis(aa).slice(0, 1);
       return first + strSliceRemove + "...";
     } else {
       return str.slice(0, 6) + "...";
@@ -58,8 +62,11 @@ const GroupList = () => {
       return str;
     } else if (checkEmojis(str)) {
       const first = str.slice(0, 7);
+      if (checkEmojis(first[6])) {
+        removeEmojis(first[6]);
+      }
       const aa = str.slice(7, 12);
-      const strSliceRemove = removeEmojis(aa);
+      const strSliceRemove = removeEmojis(aa).slice(0, 1);
       return first + strSliceRemove + "...";
     } else {
       return str.slice(0, 8) + "...";
